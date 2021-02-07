a0461c207bdb80f3d0d520bbed8ffb4d78a9d3d6
MOBUMIN <kiju23@naver.com>
2021-02-07 23:38:46 +0900

    Nav bar 버튼 추가
    * RaccoonList에 헤더 추가
    * Navbar에 들어갈 버튼 추가

--- src/components/page/RaccoonList.js
+++ src/components/page/RaccoonList.js
@@ -1,33 +1,36 @@
 import React from 'react';
 import { Grid, Button } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import { ArrowDropDown } from '@material-ui/icons';
-
-const useStyles = makeStyles((theme) => ({
-	createBtn: {
-		border: 'solid 1.5pt white',
-	},
-}));
+import { ArrowDropDown, AccountCircle } from '@material-ui/icons';
+import Header from '../UI/Header';
 
 const RaccoonList = () => {
-	const classes = useStyles();
 	return (
-		<Grid container linespacing={2}>
-			<Button
-				variant="contained"
-				color="primary"
-				className={classes.createBtn}
-				disableElevation
-			>
-				CREATE
-			</Button>
-			<Button
-				variant="contained"
-				color="default"
-				startIcon={}
-				endIcon={<ArrowDropDown fontSize="large" />}
-			></Button>
-		</Grid>
+		<>
+			<Header
+				right={
+					<div>
+						<Button
+							variant="contained"
+							color="primary"
+							className={`${'NavBtn'} ${'createBtn'}`}
+							disableElevation
+						>
+							CREATE
+						</Button>
+						<Button
+							variant="contained"
+							color="default"
+							className="NavBtn"
+							endIcon={<ArrowDropDown fontSize="large" />}
+							size="small"
+						>
+							<AccountCircle />
+						</Button>
+					</div>
+				}
+			/>
+			<Grid container linespacing={2}></Grid>
+		</>
 	);
 };
 
