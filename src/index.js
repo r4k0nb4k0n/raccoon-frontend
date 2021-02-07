4bf83fdecd8aac1ef43e2fbf223be89793dacf34
hyechoi <hyechoi@student.42seoul.kr>
2021-02-08 00:39:53 +0900

    기본 배경색 main.black.main 지정

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { MuiThemeProvider } from '@material-ui/core/styles';
+import { CssBaseline } from '@material-ui/core';
 import theme from './theme';
 
 ReactDOM.render(
 	<MuiThemeProvider theme={theme}>
+		<CssBaseline />
 		<App />
 	</MuiThemeProvider>,
 	document.getElementById('root')
