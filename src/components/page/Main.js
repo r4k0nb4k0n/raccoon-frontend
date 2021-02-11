6941ba2fc3dafc9e5a8c338046a206cd69863ef6
hyechoi <hyechoi@student.42seoul.kr>
2021-02-11 09:36:04 +0900

    Main 페이지 디자인 적용

--- src/components/page/Main.js
+++ src/components/page/Main.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button, Grid } from '@material-ui/core';
+import { Header } from '../UI';
 
 const Main = () => {
 	return (
-		<Grid className="main">
-			<Link to={`/create`}>
-				<Button className="main-button">방 생성</Button>
-			</Link>
-			<Link to={`/enter`}>
-				<Button className="main-button">방 입장</Button>
-			</Link>
-		</Grid>
+		<React.Fragment>
+			<Header />
+			<Grid container spacing={3} className="main">
+				<Grid item>
+					<Link to={`/enter`}>
+						<Button className="enterButton" size="large">
+							PLAY
+						</Button>
+					</Link>
+				</Grid>
+				<Grid item>
+					<Link to={`/create`}>
+						<Button className="createButton" size="large">
+							HOST
+						</Button>
+					</Link>
+				</Grid>
+			</Grid>
+		</React.Fragment>
 	);
 };
 
